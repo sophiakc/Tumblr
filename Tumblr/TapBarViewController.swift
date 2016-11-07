b613654cc4489cf447c1809be15bf5234ae74a3c
sophiakc <sophiakc@gmail.com>
2016-11-07 11:12:54 -0800

    milestone 2 COMPLETED

---
+++ Tumblr/TapBarViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TapBarViewController.swift
+//  Tumblr
+//
+//  Created by Sophia KC on 07/11/2016.
+//  Copyright © 2016 Sophia KC. All rights reserved.
+//
+
+import UIKit
+
+class TapBarViewController: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var contentView: UIView!
+    
+    
+    // Variables
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    
+    
+    
+
+    
+
+}
