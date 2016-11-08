cc442741e88b3d5b043b35b45a602c9064ad2cb1
sophiakc <sophiakc@gmail.com>
2016-11-08 02:24:15 -0800

    milestone #10 completed

---
+++ Tumblr/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Tumblr
+//
+//  Created by Sophia KC on 08/11/2016.
+//  Copyright © 2016 Sophia KC. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+    @IBAction func didTapCancel(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
